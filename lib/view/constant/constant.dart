4e82a1b98e04ce00cc6174982f844c091e258f65
Zakiyuddin Rahman <zakiyuddinr@gmail.com>
2022-11-25 22:13:37 +0700

    init home feature

--- lib/view/constant/constant.dart
+++ lib/view/constant/constant.dart
@@ -1,4 +1,5 @@
 const assets = "assets/";
+const icons = "assets/icons/";
 const appLogo = "${assets}app-logo.svg";
 
 const imageSignUp = "${assets}Image-Signup.svg";
@@ -7,3 +8,15 @@ const imageForgotPassword = "${assets}Image-Forgot-Password.svg";
 
 const imageCheckbox = "${assets}checkbox.svg";
 
+const iconHome = "${icons}home_outlined.svg";
+const iconHomeFilled = "${icons}home_filled.svg";
+const iconInvoice = "${icons}invoice_outlined.svg";
+const iconInvoiceFilled = "${icons}invoice_filled.svg";
+const iconReport = "${icons}report_outlined.svg";
+const iconReportFilled = "${icons}report_filled.svg";
+const iconProfile = "${icons}profile_outlined.svg";
+const iconProfileFilled = "${icons}profile_filled.svg";
+const iconNotif = "${icons}notification_outlined.svg";
+const iconNotifFilled = "${icons}notification_filled.svg";
+const iconChat = "${icons}chat_outlined.svg";
+const iconChatFilled = "${icons}chat_filled.svg";
