fd0efd87c6e2419416ef0e460dc418f709d8656a
Ade Febrian <59857124+Adefebrian@users.noreply.github.com>
2022-12-09 18:14:34 +0700

    menambahkan integrasi verifikasi

--- lib/viewmodel/auth_provider.dart
+++ lib/viewmodel/auth_provider.dart
@@ -1,35 +1,107 @@
-import 'package:flutter/material.dart';
+import 'dart:ffi';
+import 'dart:math';
+
+import 'package:dio/dio.dart';
+import 'package:myinvoice/data/endpoint/endpoint.dart';
 import 'package:myinvoice/models/auth/auth_model.dart';
-import 'package:myinvoice/services/auth_services.dart';
+import 'package:platform_device_id/platform_device_id.dart';
 
-class AuthProvider extends ChangeNotifier {
+class AuthService {
+  static Future<SignInResponse> signIn(String email, String password) async {
+    String? deviceId = await PlatformDeviceId.getDeviceId;
+    print(deviceId);
+    try {
+      print(Endpoint.login);
+      final res = await Dio().post(Endpoint.login,
+          data: {"email": email, "password": password, "device_id": deviceId});
+      print(res.data);
+      print(res.statusCode);
 
-  bool isLoading = false;
+      return SignInResponse(
+          statusCode: res.statusCode, data: res.data, error: res.data);
+    } on DioError catch (e) {
+      print(e.response!.data);
 
-  String? emailSignUp;
-  Future<SignInResponse?> signIn(String email, String password) async {
-    isLoading = true;
-    notifyListeners();
-
-    final response = await AuthService.signIn(email, password);
-    
-
-    isLoading = false;
-    notifyListeners();
-
-    return response;
+      if (e.response!.statusCode! > 500) {
+        return SignInResponse(
+          statusCode: e.response?.statusCode,
+          data: {
+            'error': {'message': 'Server error'}
+          },
+        );
+      } else {
+        return SignInResponse(
+            statusCode: e.response?.statusCode,
+            data: e.response?.data,
+            error: e.response?.data);
+      }
+    }
   }
 
-  Future<SignUpResponse?> signUp(
-      String name, String email, String password) async {
-    isLoading = true;
-    notifyListeners();
-    final result = await AuthService.signUp(name, email, password);
-    emailSignUp = email;
+  static Future<SignUpResponse> signUp(
+      String fullname, String email, String password) async {
+    try {
+      final res = await Dio().post(
+        Endpoint.register,
+        data: {"email": email, "password": password, "full_name": fullname},
+      );
 
-    isLoading = false;
-    notifyListeners();
+      print(res.data);
+      print(res.statusCode);
 
-    return result;
+      return SignUpResponse(
+          statusCode: res.statusCode,
+          data: res.data['data'],
+          error: res.data['error']);
+    } on DioError catch (e) {
+      if (e.response!.statusCode! > 500) {
+        return SignUpResponse(
+            statusCode: e.response?.statusCode,
+            error: {'message': 'Server error'});
+      } else {
+        print(e.response?.data);
+        return SignUpResponse(
+            statusCode: e.response?.statusCode,
+            error: e.response?.data['error']);
+      }
+    }
+  }
+
+  static Future<Verification> verifEmail(String email, String code) async {
+    try {
+      final res = await Dio()
+          .post(Endpoint.verification, data: {'email': email, 'code': code});
+      print(res.data);
+
+      return Verification(
+          statusCode: res.statusCode,
+          data: res.data['data'],
+          error: res.data['error']);
+    } on DioError catch (e) {
+      if (e.response!.statusCode! > 500) {
+        return Verification(
+            statusCode: e.response?.statusCode,
+            error: {'message': 'Server error'});
+      } else {
+        return Verification(
+            statusCode: e.response?.statusCode,
+            data: e.response?.data['data'],
+            error: e.response?.data['error']);
+      }
+    }
+  }
+
+  static Future<bool?> resetPassword(String email) async {
+    try {
+      final res = await Dio().post(Endpoint.resetPassword, data: {
+        'email': email,
+      });
+
+      if (res.statusCode == 200) {
+        return true;
+      }
+    } catch (e) {
+      return false;
+    }
   }
 }
