f0cfda964ea6837498de083de353fd31166a410f
Ade Febrian <59857124+Adefebrian@users.noreply.github.com>
2022-12-09 13:29:19 +0700

    perbaikan otp_screen

--- lib/viewmodel/auth_provider.dart
+++ lib/viewmodel/auth_provider.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:myinvoice/models/auth/auth_model.dart';
-import 'package:myinvoice/services/auth_services.dart';
+import 'package:myinvoice/models/auth/auth_response.dart';
+import 'package:myinvoice/repository/api_repository.dart';
+import 'package:myinvoice/services/auth_service.dart';
 
 class AuthProvider extends ChangeNotifier {
+
   bool isLoading = false;
+
   String? emailSignUp;
   Future<SignInResponse?> signIn(String email, String password) async {
     isLoading = true;
     notifyListeners();
 
-    final response = await ApiRepository.signIn(email, password);
+    final response = await AuthService.signIn(email, password);
+    
 
     isLoading = false;
     notifyListeners();
@@ -20,14 +24,13 @@ class AuthProvider extends ChangeNotifier {
   Future<SignUpResponse?> signUp(
       String name, String email, String password) async {
     isLoading = true;
-    emailSignUp = email;
     notifyListeners();
-
-    final response = await ApiRepository.signUp(name, email, password);
+    final result = await AuthService.signUp(name, email, password);
+    emailSignUp = email;
 
     isLoading = false;
     notifyListeners();
 
-    return response;
+    return result;
   }
 }
