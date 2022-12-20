d52572e63a52fe21b4b8b49d7eaca86cfab0de52
Ade Febrian <59857124+Adefebrian@users.noreply.github.com>
2022-12-20 20:27:53 +0700

    melakukan pengahapusan unused package dan code pada file dan folder splash

--- lib/view/screens/splash/splash_screen.dart
+++ lib/view/screens/splash/splash_screen.dart
@@ -1,24 +1,18 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'dart:async';
-import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:myinvoice/data/pref.dart';
-import 'package:myinvoice/services/customer_service.dart';
 import 'package:myinvoice/view/constant/constant.dart';
 import 'package:myinvoice/view/screens/auth/signin_screen.dart';
-import 'package:myinvoice/view/screens/auth/signup_screen.dart';
 import 'package:myinvoice/view/screens/home/home_screen.dart';
 import 'package:myinvoice/view/styles/styles.dart';
 import 'package:myinvoice/viewmodel/profile_provider.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-import '../../../services/invoice_service.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
